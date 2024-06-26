3c9a1a8be7589ab424898d9641a07fa497569484
BPdog <dkstl3344@gmail.com>
2024-06-26 14:50:23 +0900

    Updates

---
+++ precache-manifest.eb409f48b517e1de3ba09c9146ebc633.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f46d84f64fd9c0933ad1488eb508519c",
+    "url": "/HBD/index.html"
+  },
+  {
+    "revision": "da25989bd0886fa4e97d",
+    "url": "/HBD/static/css/main.d3e23b7a.chunk.css"
+  },
+  {
+    "revision": "0379c21d27adbf5e28d2",
+    "url": "/HBD/static/js/2.f2de23e1.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/HBD/static/js/2.f2de23e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da25989bd0886fa4e97d",
+    "url": "/HBD/static/js/main.33282285.chunk.js"
+  },
+  {
+    "revision": "8ca16104b466a85924f8",
+    "url": "/HBD/static/js/runtime-main.039fa170.js"
+  }
+]);
